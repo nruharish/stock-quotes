6cb6af3ee403881db3e3d59d3f7104250a0b1c4d
Harish BS <bs.harish@gmail.com>
2023-12-13 17:58:19 +0530

    url generation

--- downloader/nse.py
+++ downloader/nse.py
@@ -1,3 +1,13 @@
-
 class NSE:
-    pass
+    def __init__(self, dt):
+        self.dt = dt
+        self.NSE_URL = "https://archives.nseindia.com/content/historical/EQUITIES/%Y/{0}/cm%d{1}%Ybhav.csv.zip"
+        self.nse_url = ''
+
+    def url(self):
+        # https://archives.nseindia.com/content/historical/EQUITIES/2020/MAY/cm17MAY2022bhav.csv.zip
+        month = self.dt.strftime('%b').upper()
+        self.nse_url = self.NSE_URL.format(month, month)
+        self.nse_url = self.dt.strftime(self.nse_url)
+        print(self.nse_url)
+    
--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,9 +1,17 @@
 from downloader.nse import NSE
+import datetime
 
 
-def test_download():
-    nse = NSE()
+def test_download_date():
+    dt = datetime.datetime.now()
+    nse = NSE(dt)
+    assert nse.dt == dt
 
-    
-def test_another_sum():
-    assert 3 + 2 == 5
+
+def test_download_url():
+    dt = datetime.datetime(2022, 5, 17)
+    nse = NSE(dt)
+    nse.url()
+    assert nse.nse_url == 'https://archives.nseindia.com/content/historical/EQUITIES/2022/MAY/cm17MAY2022bhav.csv.zip'
+
+
